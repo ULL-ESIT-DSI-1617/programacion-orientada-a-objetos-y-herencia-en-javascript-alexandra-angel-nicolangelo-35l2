0dfb00602b336e745aab68f8906e783e85b0c1e5
nico_apache <nico10907@hotmail.com>
2017-03-26 11:28:51 +0000

    arreglando de K F y K C

--- temperature.js
+++ temperature.js
@@ -15,11 +15,11 @@ function select() {
       num = parseFloat(num);
       if (type == 'c' || type == 'C') {
         result = (num * 9/5)+32;
-        result = result.toFixed(1)+" Farenheit ";
+        result = result.toFixed(1)+" Farenheit "+type;
       }
       else{
         result = (num - 32)*5/9;
-        result = result.toFixed(1)+" Celsius ";
+        result = result.toFixed(1)+" Celsius "+type;
       }
       converted.innerHTML = result;
     // }
@@ -36,17 +36,22 @@ function calculate() {
   
   var m = temp.match(regexp);
   switch(m[2]) {
+    case 'C':
     case 'c':
         select();
         break;
+    case 'F':
     case 'f':
         select();
         break;
+    case 'K C':
+    case 'k c':
+    case 'K F':
     case 'k f':
         modificacion();
         break;
     default:
-        converted.innerHTML = "error! "+m[2];
+        converted.innerHTML = "error! only (C/c, F/f, K F/k f, K C/k c) You type: "+m[2];
   }
   
 }
@@ -61,26 +66,19 @@ function modificacion() {
   var type = m[2];
   
   if (m) {
-    if (type == "k f"){
+    if (type == "k f" | type == "K F"){
       num = parseFloat(num);
       result = 1.8 * (num - 273.15) + 32;
-      result = result.toFixed(1)+" Farenheit";
+      result = result.toFixed(1)+" Farenheit "+type;
       converted.innerHTML = result;
-    }else if (type == "k c"){
+    }else if (type == "k c"| type == "K C"){
       num = parseFloat(num);
       result = (num-273.15);
-      result = result.toFixed(1)+" Celsius";
+      result = result.toFixed(1)+" Celsius "+type;
       converted.innerHTML = result;
-    }else if (type == 'c' || type == 'C') {
-        result = (num * 9/5)+32;
-        result = result.toFixed(1)+" Farenheit";
-      }
-      else {
-        result = (num - 32)*5/9;
-        result = result.toFixed(1)+" Celsius";
-      }
-      converted.innerHTML = result;
+    }
+    converted.innerHTML = result;
   }else {
-  converted.innerHTML = "ERROR! Prueba '-4.2C' '373k f' '250k c'";
+  converted.innerHTML = "error! only (C/c, F/f, K F/k f, K C/k c) You type: "+type;
   }
 }
