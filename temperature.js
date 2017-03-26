24b63d5c9a57aef50fa8f21f9948f418e0953944
nico_apache <nico10907@hotmail.com>
2017-03-26 11:36:09 +0000

    index modificado

--- temperature.js
+++ temperature.js
@@ -5,34 +5,30 @@ function select() {
   var result;
   var temp = original.value;
   var regexp = /([-+]?\d+(?:\.\d*)?)\s*([fFcC])/;
-  // var regexp = /([-+]?\d+(?:\.\d*)?)\s*([fFcC])/;
   var m = temp.match(regexp);
-  // converted.innerHTML = "ERROR! "+m[2];
-    // if (m) {
+    if (m) {
       var num = m[1];
       var type = m[2];
-      // converted.innerHTML = "ERROR! "+type;
       num = parseFloat(num);
       if (type == 'c' || type == 'C') {
         result = (num * 9/5)+32;
-        result = result.toFixed(1)+" Farenheit "+type;
+        result = result.toFixed(1)+" Farenheit ";
       }
       else{
         result = (num - 32)*5/9;
-        result = result.toFixed(1)+" Celsius "+type;
+        result = result.toFixed(1)+" Celsius ";
       }
       converted.innerHTML = result;
-    // }
-    // else {
-    //   converted.innerHTML = "ERROR! Try something like '-4.2C' instead ";
-    // }
+    }
+    else {
+      converted.innerHTML = "error! only (C/c, F/f, K F/k f, K C/k c) You type: "+m[2];
+    }
 }
 
 function calculate() {
   var result;
   var temp = original.value;
   var regexp = /([-+]?\d+(?:\.\d*)?)\s*(k\sf|K\sF|k\sc|K\sC|([fFcC]))/;
-  // var regexp = /([-+]?\d+(?:\.\d*)?)\s*([fFcC])/;
   
   var m = temp.match(regexp);
   switch(m[2]) {
@@ -59,7 +55,6 @@ function calculate() {
 function modificacion() {
   var result;
   var temp = original.value;
-  // var regexp = /([-+]?\d+(?:\.\d*)?)\s*(k\sf|K\sF|k\sc|K\sC|([fFcC]))/;
   var regexp = /([-+]?\d+(?:\.\d*)?)\s*(k\sf|K\sF|k\sc|K\sC)/;
   var m = temp.match(regexp);
   var num = m[1];
@@ -69,13 +64,15 @@ function modificacion() {
     if (type == "k f" | type == "K F"){
       num = parseFloat(num);
       result = 1.8 * (num - 273.15) + 32;
-      result = result.toFixed(1)+" Farenheit "+type;
+      result = result.toFixed(1)+" Farenheit ";
       converted.innerHTML = result;
     }else if (type == "k c"| type == "K C"){
       num = parseFloat(num);
       result = (num-273.15);
-      result = result.toFixed(1)+" Celsius "+type;
+      result = result.toFixed(1)+" Celsius ";
       converted.innerHTML = result;
+    }else {
+      converted.innerHTML = "error! only (C/c, F/f, K F/k f, K C/k c) You type: "+type;
     }
     converted.innerHTML = result;
   }else {
