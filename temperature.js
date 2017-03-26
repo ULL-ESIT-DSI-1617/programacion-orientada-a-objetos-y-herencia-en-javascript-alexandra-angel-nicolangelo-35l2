036737eb63658a34c2d85db4b8306b2ff428bb25
wiwihyuga <angel_luis_tf@hotmail.com>
2017-03-26 22:12:25 +0000

    creando herencias

--- temperature.js
+++ temperature.js
@@ -1,6 +1,8 @@
 "use strict"; // Use ECMAScript 5 strict mode in browsers that support it
 
-
+function Medida(){
+  var result;
+}
 function select() {
   var result;
   var temp = original.value;
@@ -25,10 +27,11 @@ function select() {
     }
 }
 
-function calculate() {
+Medida.prototype.temperatura;
+Medida.prototype.temperatura = function() {
   var result;
   var temp = original.value;
-  var regexp = /([-+]?\d+(?:\.\d*)?)\s*(k\sf|K\sF|k\sc|K\sC|c\sk|C\sK|f\sk|F\sK|([fFcC]))/;
+  var regexp = /([-+]?\d+(?:\.\d*)?)\s*(k\sf|K\sF|k\sc|K\sC|([fFcC]))/;
   
   var m = temp.match(regexp);
   switch(m[2]) {
@@ -41,10 +44,6 @@ function calculate() {
         select();
         break;
     case 'K C':
-    case 'F K':
-    case 'f k':
-    case 'C K':
-    case 'c k':
     case 'k c':
     case 'K F':
     case 'k f':
@@ -55,11 +54,32 @@ function calculate() {
   }
   
 }
+function calculate() {
+  var resultado= new Medida();
+  var temp = original.value;
+  var regexp = /([-+]?\d+(?:\.\d*)?)\s*([fFcC])/;
+  var m = temp.match(regexp);
+  switch(m[2]) {
+    case 'C':
+    case 'c':
+    case 'F':
+    case 'f':
+    case 'K C':
+    case 'k c':
+    case 'K F':
+    case 'k f':
+        resultado.temperatura();
+        break;
+    default:
+        converted.innerHTML = "error! only (C/c, F/f, K F/k f, K C/k c) You type: "+m[2];
+  }
+  
+}
 
 function modificacion() {
   var result;
   var temp = original.value;
-  var regexp = /([-+]?\d+(?:\.\d*)?)\s*(k\sf|K\sF|k\sc|K\sC|c\sk|C\sK|f\sk|F\sK)/;
+  var regexp = /([-+]?\d+(?:\.\d*)?)\s*(k\sf|K\sF|k\sc|K\sC)/;
   var m = temp.match(regexp);
   var num = m[1];
   var type = m[2];
@@ -75,19 +95,7 @@ function modificacion() {
       result = (num-273.15);
       result = result.toFixed(1)+" Celsius ";
       converted.innerHTML = result;
-    }else if (type == "c k"| type == "C K"){
-      num = parseFloat(num);
-      result = (num+273.15);
-      result = result.toFixed(1)+" Kelvin ";
-      converted.innerHTML = result;
-    }else if (type == "f k"| type == "F K"){
-      num = parseFloat(num);
-      result = ((num - 32)/1.8)+273.15;
-      result = result.toFixed(1)+" Kelvin ";
-      converted.innerHTML = result;
-    }
-    
-    else {
+    }else {
       converted.innerHTML = "error! only (C/c, F/f, K F/k f, K C/k c) You type: "+type;
     }
     converted.innerHTML = result;
